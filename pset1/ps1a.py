498e9116cb7853c05d85325c690dfe182741447d
lilyroses <lilyrosestracke.dev@gmail.com>
2025-06-26 23:45:14 -0700

    "Complete ps1a.py, ps1b.py, and ps1c.py for pset1."

--- pset1/ps1a.py
+++ pset1/ps1a.py
@@ -1,13 +1,14 @@
-## 6.100A PSet 1: Part A
-## Name:
-## Time Spent:
-## Collaborators:
+# 6.100A PSet 1: Part A
+# Name: Lily Rose Stracke
+# Time Spent: > 1hr
+# Collaborators:
 
 ##################################################################################
 ## Get user input for yearly_salary, portion_saved and cost_of_dream_home below ##
 ##################################################################################
 yearly_salary = float(input("Enter your yearly salary: "))
-portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+portion_saved = float(
+    input("Enter the percent of your salary to save, as a decimal: "))
 cost_of_dream_home = float(input("Enter the cost of your dream home: "))
 
 #########################################################################
@@ -19,13 +20,13 @@ amount_saved = 0
 months = 0
 
 ###############################################################################################
-## Determine how many months it would take to get the down payment for your dream home below ## 
+## Determine how many months it would take to get the down payment for your dream home below ##
 ###############################################################################################
 while amount_saved < portion_down_payment:
-  months += 1
-  amount_saved += (amount_saved * (r/12))
-  amount_saved += ((yearly_salary/12) * portion_saved)
-  if amount_saved >= portion_down_payment:
-    break
+    months += 1
+    amount_saved += (amount_saved * (r/12))
+    amount_saved += ((yearly_salary/12) * portion_saved)
+    if amount_saved >= portion_down_payment:
+        break
 
 print(f"Number of months: {months}")
